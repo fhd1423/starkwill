61a529324a6f57930dc80376e61e0148870dcee0
Rohith Venkatesan <rohithvenkatesan142@gmail.com>
2024-02-25 18:53:54 -0700

    blank frontend

--- deploy-invoke.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-output=$(starknet deploy --contract artifacts/InsuranceNFT.json --inputs 381295704707908511112352986844521317 314863540297 1 140152554740597502879502332212244787760 20 20  --network alpha-goerli)
-echo $output
-deploy_tx_id=$(echo $output | sed -r "s/.*Transaction ID: (\w*).*/\1/")
-address=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
-echo "{\"address\" : \"$address\"}" > testObj.json
-#starknet call --function name --network alpha-goerli --address $address --abi artifacts/abis/InsuranceNFT.json
--- python-sign.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ./cairo_venv/bin/activate
-python starkware-signing-script.py $1 $2
